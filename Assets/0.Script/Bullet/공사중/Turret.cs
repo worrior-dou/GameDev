03ecca39945dafed16b26ece647e4087f7a6bc3e
terrapinam <120235378+worrior-dou@users.noreply.github.com>
2023-05-01 22:27:31 +0900

    터렛(FirePos) 작동
    - EnemyA~C 프리팹에 포함되면서, 터렛 소스 수정함
        👉 Turret은 플레이어를 쳐다보는 기능을 함
        👉 플레이어의 위치를 TurretCont에서 받고, Turret.cs에 있는 LookPlayer함수를 실행시킴

---
+++ Assets/0.Script/Bullet/공사중/Turret.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    Transform playerPos;
+
+    public static Turret Instance;
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void LookPlayer(Transform playerPos)
+    {
+        //플레이어 쳐다보기
+        {
+            Vector2 vec = transform.position - playerPos.position;
+            float angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        }
+    }
+}
